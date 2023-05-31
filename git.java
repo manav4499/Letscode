dd4a480cf468b31a4b45eab6216484877a13eab8
Manav Chaudary <manavchaudhary462@gmail.com>
2023-05-31 20:36:15 +0530

    adding diff.java

--- git.java
+++ git.java
@@ -3,7 +3,7 @@ public class git {
     public static void main(String[] args) {
         int a =8;  
         int b =3;
-        System.out.println(a+b);  
+        System.out.println("the sum is " + (a+b));  
     }
     
 
